bfd29fd3d97b5a6efe40fcdeffcca3601a231ab7
Karina Apaeva <karina.apaeva96@gmail.com>
2024-01-29 23:16:35 +0100

    added homework

---
+++ models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint('id'),
+    )
